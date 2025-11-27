064eb4d3fe95adc1d77e73211b74f651a8d95f1e
Agahozo3 <creliaagahzo3@gmail.com>
2025-11-27 15:07:53 +0200

    Initial commit of frontend

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  preview: {
+    host: true, // allows external access
+    port: 5173, // must match your start script
+    allowedHosts: [
+      "ist-procurepay-frontend-production.up.railway.app"
+    ]
+  }
 })
